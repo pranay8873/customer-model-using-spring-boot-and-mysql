480d4eb7a466acc23fd465be76f00189b8b0bcd0
pranay8873 <pranaypuyaati@gmail.com>
2026-06-25 20:07:07 +0530

    added maven dependency for model mapping

--- src/main/java/com/ecomerce/customer/model/repository/customerrepo.java
+++ src/main/java/com/ecomerce/customer/model/repository/customerrepo.java
@@ -1,7 +1,10 @@
 package com.ecomerce.customer.model.repository;
 
 import com.ecomerce.customer.model.entity.customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface customerrepo extends jpaRepository<customer,Long>{
+public interface customerrepo extends JpaRepository<customer,Long> {
+
+    customer findByEmail(String email);
 
 }
